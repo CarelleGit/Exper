ade9dcf531551f5e9ae44147b30973e5577b35b1
Carelle <steamtik@gmail.com>
2018-07-10 12:56:24 -0700

    the PlaceHolder Ring now appears

---
+++ Assets/Scripts/MouseHovering.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHovering : MonoBehaviour
+{
+
+    private void OnPointerEnter()
+    {
+        Debug.Log(gameObject.name + " Detected");
+    }
+    private void OnMouseExit()
+    {
+        Debug.Log(gameObject.name + " Lost");
+    }
+}
+    
---
+++ Assets/Scripts/SkillTreeRing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillTreeRing : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject skillTreeRing;
+    public string layer;
+    private Ray ray;
+    private RaycastHit2D raycast;
+
+    private Vector2 skillTree;
+
+    private bool isTrue = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (!isTrue)
+            {
+                ActivateSkillTree();
+            }
+            else
+            {
+                DeactivateSkillTree();
+            }
+        }
+    }
+
+    void ActivateSkillTree() //sets the GameObject true
+    {
+        skillTreeRing.SetActive(true);
+        isTrue = true;
+
+    }
+    void DeactivateSkillTree() //sets the GameObject false
+    {
+        skillTreeRing.SetActive(false);
+        isTrue = false;
+    }
+}
